d216ece80ea969388a05d2a5b3aeed3b0ffce00f
François-Régis Degott <francoisregisdegott@eaton.com>
2025-10-08 15:30:09 +0200

    convert.h : add #include <cstdint>

--- fty/convert.h
+++ fty/convert.h
@@ -13,11 +13,14 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     ========================================================================
 */
+
 #pragma once
+
 #include "traits.h"
 #include <sstream>
 #include <string>
 #include <type_traits>
+#include <cstdint>
 
 namespace fty {
 
